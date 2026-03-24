fdd7561ff0a9d5587c015f0597e84c9fdb8649e0
pclijten <pclijten@gmail.com>
2026-03-24 11:00:17 +0100

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,6 +1,6 @@
 import { initializeApp, getApps, getApp } from 'https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js';
-import { getAuth }      from 'https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js';
+import { getAuth }                         from 'https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js';
+import { getFirestore }                    from 'https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js';
 
 const firebaseConfig = {
   apiKey:            "AIzaSyDkrkHrPW4pPu0_ZVKGma-AVF4-gTtk9R8",
